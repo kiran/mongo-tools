a5ba1fb27ac8cf59c70b14128204151ae597f8c5
Alex Coomans <alex@alexcoomans.com>
2013-01-19 15:57:18 -0800

    Rewrite all of the DB access permission code and add in read only options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  helper_method :current_collection_name, :current_database_name, :current_document_id
+  helper_method :can_read?, :can_edit?, :can_edit_collection?, :can_edit_database?, :can_read_collection?, :can_read_database?,
+                :current_collection_name, :current_database_name, :current_document_id
   
   protected
     def current_database_name
@@ -13,5 +14,43 @@ class ApplicationController < ActionController::Base
     
     def current_document_id
       current_collection_name && params[:controller] == "explorer/documents" ? params[:id] : nil
+    end
+
+    def can_read?(db = nil, coll = nil)
+      db ||= current_database_name
+      coll ||= current_collection_name
+      return db && coll ? can_read_collection?(db, coll) : can_read_database?(db)
+    end
+    
+    def can_edit?(db = nil, coll = nil)
+      db ||= current_database_name
+      coll ||= current_collection_name
+      return db && coll ? can_edit_collection?(db, coll) : can_read_database?(db)
+    end
+    
+    def can_read_database?(db = nil)
+      db ||= current_database_name
+      raise Exception, "Possible bug with DB security" unless db
+      !DATABASE_BLACKLIST.include?(db)
+    end
+    
+    def can_edit_database?(db = nil)
+      db ||= current_database_name
+      raise Exception, "Possible bug with DB security" unless db
+      can_read_database?(db) && !DATABASE_READ_ONLY.include?(db)
+    end
+    
+    def can_read_collection?(db = nil, coll = nil)
+      db ||= current_database_name
+      coll ||= current_collection_name
+      raise Exception, "Possible bug with DB security" unless db && coll
+      can_read_database?(db) && !COLLECTION_BLACKLIST.include?(coll)
+    end
+    
+    def can_edit_collection?(db = nil, coll = nil)
+      db ||= current_database_name
+      coll ||= current_collection_name
+      raise Exception, "Possible bug with DB security" unless db && coll
+      can_edit_database?(db) && can_read_collection?(db, coll) && !COLLECTION_READ_ONLY.include?(coll)      
     end
 end
