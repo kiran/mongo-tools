3458ab9b5ec5dd0dd5fc5c5198235f0e97dd8042
Adrian Maceiras <amac425@utexas.edu>
2013-01-23 10:47:54 -0600

    Started of tabbed query interfacewq

--- app/assets/javascripts/explorer/collections.js
+++ app/assets/javascripts/explorer/collections.js
@@ -58,21 +58,19 @@ $(function () {
     return false;
   });
 
-
   window.onload=function() {
-
     // get tab container
-    var container = document.getElementById("tabContainer");
-    // set current tab
+    var container = document.getElementById("tabs-container");
     var navitem = container.querySelector(".tabs ul li");
-    //store which tab we are on
-    var ident = navitem.id.split("_")[1];
+
+    //split on r of tab-header###
+    var ident = navitem.id.split("r")[1];
     navitem.parentNode.setAttribute("data-current",ident);
     //set current tab with class of activetabheader
     navitem.setAttribute("class","tabActiveHeader");
 
     //hide two tab contents we don't need
-    var pages = container.querySelectorAll(".tabpage");
+    var pages = container.querySelectorAll(".tab-page");
     for (var i = 1; i < pages.length; i++) {
       pages[i].style.display="none";
     }
@@ -88,13 +86,14 @@ $(function () {
   function displayPage() {
     var current = this.parentNode.getAttribute("data-current");
     //remove class of activetabheader and hide old contents
-    document.getElementById("tabHeader_" + current).removeAttribute("class");
-    document.getElementById("tabpage_" + current).style.display="none";
+    document.getElementById("tab-header" + current).removeAttribute("class");
+    document.getElementById("tab-page" + current).style.display="none";
 
-    var ident = this.id.split("_")[1];
+    //split on r of tab-header###
+    var ident = this.id.split("r")[1];
     //add class of activetabheader to new active tab and show contents
     this.setAttribute("class","tabActiveHeader");
-    document.getElementById("tabpage_" + ident).style.display="block";
+    document.getElementById("tab-page" + ident).style.display="block";
     this.parentNode.setAttribute("data-current",ident);
   }
 
