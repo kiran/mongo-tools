66cb6b018be029c68f630b5c1728713e2f852341
Peter Halliday <phalliday@excelsiorsystems.net>
2013-01-30 23:27:11 -0500

    Adding ability for Edit Functionality to work.

--- app/controllers/explorer/documents_controller.rb
+++ app/controllers/explorer/documents_controller.rb
@@ -13,12 +13,31 @@ class Explorer::DocumentsController < ExplorerController
     db = conn.db(current_database_name)
     coll = db.collection(current_collection_name)
     json = JSON.parse(params[:query])
+    if json.has_key?('_id')
+      json["_id"] = BSON::ObjectId(json["_id"])
+    end
     coll.insert(json)
     redirect_to explorer_collection_path(current_database_name, current_collection_name)
   end
   
   def new
     new_id = BSON::ObjectId.new
-    @query = "{_id: ObjectId(\""+new_id+"\")}"
+    @query = "{_id: ObjectId(\""+new_id.to_s+"\")}"
+  end
+  
+  def update
+    puts "In update controller"
+    conn = MongoMapper.connection
+    db = conn.db(current_database_name)
+    coll = db.collection(current_collection_name)
+    json = JSON.parse(params[:query])
+    id = nil
+    if json.has_key?('_id')
+      id = BSON::ObjectId(json["_id"])
+      json.delete("_id")
+    end
+    
+    coll.update({'_id' => id}, json)
+    redirect_to explorer_collection_document_path(current_database_name, current_collection_name)
   end
 end
