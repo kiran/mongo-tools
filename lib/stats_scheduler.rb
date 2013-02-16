d2e4e181a2b5722004a9db197ad72f6825042104
kiran-b <kiranb@mit.edu>
2013-02-15 22:21:49 -0500

    first draft of the background statistics task

---
+++ lib/stats_scheduler.rb
@@ -0,0 +1,55 @@
+require 'rubygems'
+require 'mongo'
+
+include Mongo
+
+class StatsScheduler
+  def initialize(server, port, stats_server, stats_port, db_name)
+    @client = MongoClient.new(server, port)
+    @stats_client = MongoClient.new(stats_server, stats_port)
+    @stats_db = @stats_client[db_name]
+    @dbs_coll = @stats_db["db_stats"]
+    @srv_coll = @stats_db["srv_stats"]
+  end
+
+  def collect_opcounts
+    dbs = @client.database_names
+    stats = @client[dbs[0]].command( { serverStatus: 1 } )
+    
+    time = stats["localTime"]
+
+    doc = {"time" => time, "stats" => stats}
+    self.scrub!(doc)
+    @srv_coll.insert(doc)
+
+    dbs.each do |db_name|
+      db = @client[db_name]
+      info = db.command( { dbStats: 1, scale: 1024 } )
+
+      doc = { "time" => time, "name"=> db_name, "stats" => info }
+      self.scrub!(doc)
+
+      @dbs_coll.insert(doc)
+    end
+  end
+  def scrub!(hash)
+    # scrubs the keys of the hash to change offending "." and "$" characters
+    q = [hash]
+    while (!q.empty?)
+      curr = q.pop()
+      curr.keys.each do |key|
+        # replace key with newkey by adding newkey and deleting old key
+        newkey = key
+        if key.include? "." or key.include? "$"
+          newkey = newkey.gsub(".", ",")
+          newkey.gsub!("$", "#")
+          curr[newkey] = curr[key]
+          curr.delete(key)
+        end
+        q << curr[newkey] if curr[newkey].is_a?(Hash)
+      end
+    end
+  end
+
+end
+
---
+++ spec/lib/stats_scheduler_spec.rb
@@ -0,0 +1,97 @@
+require "mongo"
+include Mongo
+
+require "stats_scheduler"
+
+describe StatsScheduler do
+  before do
+    test_db_name = "db_test_stats" # TODO: change this
+    @stats_scheduler = StatsScheduler.new("localhost", 27017, "localhost", 27018, test_db_name)
+    @read_client = MongoClient.new("localhost", 27017)
+
+    @stats_client = MongoClient.new("localhost", 27018)
+    @test_db = @stats_client[test_db_name]
+    @test_db.drop_collection("db_stats")
+    @test_db.drop_collection("srv_stats")
+  end
+
+  describe "#collect_opcounts" do
+    before do
+      @stats_scheduler.collect_opcounts
+    end
+
+    it "should insert 1 document into db stats collection" do
+      dbs_coll = @test_db["db_stats"]
+      dbs_coll.count.should eq(@read_client.database_names.size)
+    end 
+
+    it "should insert 1 document into server stats collection" do
+      srv_coll = @test_db["srv_stats"]
+      srv_coll.count.should eq(1)
+    end
+
+    before do
+        # TODO insert 1 thing into a db
+        @demo_db = @read_client["db_test_demo_stats"]
+        @demo_db.drop_collection("demo_coll")
+        @demo_coll = @demo_db["demo_coll"]
+    end
+
+    it "should count 1 insert on the server" do
+      @demo_coll.insert({"test"=>"hi"})
+      @stats_scheduler.collect_opcounts()
+      # puts @demo_coll.find().sort({"$natural"=>1}).limit(2);
+    end
+    
+    it "should count 1 insert on a specific database" #TODO
+    it "should count 1 insert on the server database" #TODO
+    it "should include all important/necessary stats" #TODO
+  end
+
+  describe "#scrub!" do
+    it "should scrub out . and $ in first-level keys" do 
+      test_bson = BSON::OrderedHash.new
+      test_bson[ "a.a.a." ] = "aaa"
+      test_bson[ "b$" ] = 9
+      test_bson[ "c" ] = 1.033
+
+      assert_bson = BSON::OrderedHash.new
+      assert_bson[ "a,a,a," ] = "aaa"
+      assert_bson[ "b#" ] = 9
+      assert_bson[ "c" ] = 1.033
+
+      @stats_scheduler.scrub!(test_bson)
+
+      test_bson.should eql(assert_bson)
+    end
+
+    it "should scrub out . and $ in any level key" do 
+      test_bson = BSON::OrderedHash.new
+      test_bson[ "a.a.a." ] = "aaa"
+      test_bson[ "b$" ] = 9
+      test_bson[ "c" ] = 1.033
+
+      inner_bson = BSON::OrderedHash.new
+      inner_bson[ "a.a.a.." ] = "aaa"
+      inner_bson[ "$$b$" ] = 9
+
+      test_bson['inner.level'] = inner_bson
+
+      assert_bson = BSON::OrderedHash.new
+      assert_bson[ "a,a,a," ] = "aaa"
+      assert_bson[ "b#" ] = 9
+      assert_bson[ "c" ] = 1.033
+
+      inner_bson2 = BSON::OrderedHash.new
+      inner_bson2[ "a,a,a,," ] = "aaa"
+      inner_bson2[ "##b#" ] = 9
+
+      assert_bson['inner,level'] = inner_bson2
+
+      @stats_scheduler.scrub!(test_bson)
+
+      test_bson.should eql(assert_bson)
+    end
+  end
+end
+
