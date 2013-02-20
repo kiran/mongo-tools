c05ef304f180446def2419d12428ffef41881155
kiran-b <kiranb@mit.edu>
2013-02-20 16:19:06 -0500

    refactored scheduler as a class

--- config/initializers/stats_scheduler.rb
+++ config/initializers/stats_scheduler.rb
@@ -4,28 +4,37 @@ require 'mongo'
 
 include Mongo
 
-scheduler = Rufus::Scheduler.start_new
-client = MongoClient.new("localhost", 27017)
+class StatsScheduler
+  def initialize (server, host, db_name)
+    @client = MongoClient.new(server, host)
+    @tools_db = @client[db_name]
 
-tools_db = client['mongotools']
-dbs_coll = tools_db['db_stats']
-srv_coll = tools_db['srv_stats']
-
-scheduler.every("2s") do
-  dbs = client.database_names
-  stats = client[dbs[0]].command({serverStatus: 1})
-  stats.delete("locks")
-  time = stats["localTime"]
-
-  doc = {"time" => time, "stats" => stats}
-
-  srv_coll.insert(doc)
-
-  dbs.each do |db_name|
-    db = client[db_name]
-    info = db.command({dbStats: 1, scale: 1024})
-
-    doc = {"time" => time, "name"=> db_name, "stats" => info}
-    dbs_coll.insert(doc)
+    @dbs_coll = @tools_db['db_stats']
+    @srv_coll = @tools_db['srv_stats']
   end
+
+  def collect_statistics
+    dbs = @client.database_names
+    stats = @client[dbs[0]].command({serverStatus: 1})
+    stats.delete("locks")
+    time = stats["localTime"]
+
+    doc = {"time" => time, "stats" => stats}
+
+    @srv_coll.insert(doc)
+
+    dbs.each do |db_name|
+      db = @client[db_name]
+      info = db.command({dbStats: 1, scale: 1024})
+
+      doc = {"time" => time, "name"=> db_name, "stats" => info}
+      @dbs_coll.insert(doc)
+    end
+  end
+end
+
+scheduler = Rufus::Scheduler.start_new
+stats_scheduler = StatsScheduler.new("localhost", 27017, "db_stats")
+scheduler.every("2s") do
+  stats_scheduler.collect_statistics
 end
