f948bdf1e5bd3575ecacfed732538219680088ba
kiran-b <kiranb@mit.edu>
2013-04-02 19:04:32 -0400

    setup scripts for the db + collections

--- config/initializers/stats_collector.rb
+++ config/initializers/stats_collector.rb
@@ -7,6 +7,15 @@ include Mongo
 unless ENV.has_key?('TRAVIS')
 	scheduler = Rufus::Scheduler.start_new
 
+	STATS_CONFIG = YAML.load_file("#{Rails.root}/config/stats.yml")[Rails.env]
+
+	# A capped collection has a max size and, optionally, a max number of records.
+	# Old records get pushed out by new ones once the size or max num records is reached.
+
+	# Connect to the db and set up capped collections for the statistics models
+	db = MongoClient.new(STATS_CONFIG['stats_host'], STATS_CONFIG['stats_port']).db(STATS_CONFIG['stats_db_name'])
+	coll = db.create_collection('server_status_objects', :capped => true, :size => STATS_CONFIG['size'])
+
 	# create a new server statistics object every n seconds
 	scheduler.every(STATS_CONFIG["frequency"]) do
 	  ServerStatusObject.new
