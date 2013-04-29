150df586afd8a78df5b3fc82437b01391b4f96ac
kiran-b <kiranb@mit.edu>
2013-04-28 21:25:56 -0400

    fixed the database setting in model

--- app/models/server_status_object.rb
+++ app/models/server_status_object.rb
@@ -1,7 +1,7 @@
 class ServerStatusObject
   include MongoMapper::Document
   connection(MongoConnections.stats)
-  set_database_name Settings.stats.db
+  set_database_name Settings.stats.database
 
   key :host, String
   key :timestamp, Time
@@ -14,7 +14,7 @@ class ServerStatusObject
     db = MongoMapper.connection[MongoMapper.connection.database_names[0]]
     stats = db.command( { serverStatus: 1 } )
     scrub!(stats)
-    
+
     self.timestamp = stats["localTime"]
     self.host = stats["host"]
     self.op_counters = stats["opcounters"]
@@ -46,7 +46,7 @@ class ServerStatusObject
   end
 end
 
-# format: 
+# format:
 # {
 #   "host" : "hostname:port",
 #   "opcounters" : {
